7b4ec8e1dbb221bbb049937d118d21aa364552dd
Matthew Norman <mnorman@huronconsultinggroup.com>
2020-03-25 01:16:50 -0700

    Refactor to simplify repro

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const constants = {
 	"methods": {
 		"GET": "GET",
 		"POST": "POST"
+	},
+	"headers": {
+		"contentType": "content-type"
+	},
+	"contentTypes": {
+		"applicationJson": "application/json; charset=utf-8"
 	}
 }
 
@@ -33,23 +39,29 @@ const constants = {
  * @param {RequestOptions} options https.request options.
  * @returns {Promise<RequestResult>} Resolves with the request and response object.
  */
-function makeRequestTryRepro(url, options) {
-	options = Object.assign({}, options)
-	options.method = options.method ? options.method : options.body ? constants.methods.POST : constants.methods.GET
-
+function makeRequest(url, options) {
 	return new Promise((resolve, reject) => {
 		const req = https.request(url, options, (res) => {
 			const chunks = []
 			res.on('data', (chunk) => {
 				chunks.push(chunk)
 			})
+
 			res.on('end', () => {
-				//Other 20* error codes should be handled.
 				if (res.statusCode !== constants.statusCodes.ok) {
 					reject(new Error(`Error accessing URL ${url}. StatusCode: ${res.statusCode}`))
 					return
 				}
 				res.body = Buffer.concat(chunks).toString()
+				try {
+					if (res.headers[constants.headers.contentType] === constants.contentTypes.applicationJson) {
+						res.bodyParsed = JSON.parse(res.body)
+					}
+				} catch (e) {
+					reject(new Error("Could not parse the response."))
+					return
+				}
+
 				resolve({
 					req,
 					res
@@ -65,125 +77,28 @@ function makeRequestTryRepro(url, options) {
 		})
 		req.end()
 	})
-}
-
-/**
- * @param {string} url https.request url
- * @param {RequestOptions} options https.request options.
- * @returns {Promise<RequestResult>} Resolves with the request and response object.
- */
-function makeRequestNoRepro(url, options) {
-	options = addUrlToOptions(url, options)
-	options.method = options.method ? options.method : options.body ? constants.methods.POST : constants.methods.GET
-
-	return new Promise((resolve, reject) => {
-		const req = https.request(options, (res) => {
-			const chunks = []
-			res.on('data', (chunk) => {
-				chunks.push(chunk)
-			})
-			res.on('end', () => {
-				//Other 20* error codes should be handled.
-				if (res.statusCode !== constants.statusCodes.ok) {
-					reject(new Error(`Error accessing URL ${url}. StatusCode: ${res.statusCode}`))
-					return
-				}
-				res.body = Buffer.concat(chunks).toString()
-				resolve({
-					req,
-					res
-				})
-			})
-		})
-
-		if (options.body) {
-			req.write(options.body)
-		}
-		req.on('error', (e) => {
-			reject(e)
-		})
-		req.end()
-	})
-}
-
-/**
- * @param {string} url https.request url
- * @param {RequestOptions} [options] https.request options. If not provided a new object is created. If provided, the options object will be modified.
- * @returns {RequestOptions} Options with URL parts added.
- */
-function addUrlToOptions(url, options) {
-	options = Object.assign({}, options)
-	var parsedUrl = urlModule.parse(url)
-	options.protocol = parsedUrl.protocol
-	options.host = parsedUrl.host
-	options.path = parsedUrl.path
-
-	return options
 }
 
 /**
  * Execute a GET request to some URL. Prompt for a URL.
- * @param {boolean} tryRepro If true, the code with the erroneous behavior will be used, else the non-erroneous behavior code will be run.
  */
-async function testRequest(tryRepro) {
-	// const { url, requestCount } = await getInput()
-	// if (url === undefined) {
-	// 	return
-	// }
-
-	const requestCount = 200
-	const url = "https://mpclkmndev02.huronclick.com/irb/sd/PublicCustomLayouts/request"
+async function testRequest() {
+	const url = "https://www.google.com"
 	const options = {
-		headers: {}
+		method: constants.methods.GET
 	}
 
-	//Test case: Make a request. It seems multiple have to be made to trigger the issue.
-	vscode.window.showInformationMessage(`Repro test? ${tryRepro}, Making requests: count: ${requestCount}`)
-	const requests = []
-	for (var i = 0; i < requestCount; i++) {
-		if (tryRepro) {
-			requests.push(makeRequestTryRepro(url, options))
-		} else {
-			requests.push(makeRequestNoRepro(url, options))
-		}
+	vscode.window.showInformationMessage(`Starting test`)
+	try {
+		await makeRequest(url, options)
+		vscode.window.showInformationMessage(`Request succeeded: ${url}`)
+	} catch (e) {
+		console.error(e.message)
+		console.trace(`Repro test`)
+		vscode.window.showErrorMessage(`Error occurred: ${e.message}`)
 	}
-	Promise.all(requests)
-		.then(() => {
-			vscode.window.showInformationMessage(`Repro test? ${tryRepro}, Request succeeded: ${url}`)
-		})
-		.catch(e => {
-			console.error(e.message)
-			console.trace(`Repro test? ${tryRepro}`)
-			vscode.window.showErrorMessage(`Error occurred: ${e.message}`)
-		})
-
-	async function getInput() {
-		const url = await vscode.window.showInputBox({
-			prompt: "Provide a URL to GET.",
-			value: "https://www.google.com"
-		})
-		if (url === undefined) {
-			return {}
-		}
-		let requestCount
-		do {
-			requestCount = await vscode.window.showInputBox({
-				prompt: "How many requests to make?",
-				value: "10"
-			})
-			if (requestCount === undefined) {
-				return {}
-			}
-			if (isNaN(parseInt(requestCount))) {
-				vscode.window.showWarningMessage("Request count must be a number.")
-			}
-		} while (isNaN(parseInt(requestCount)))
-
-		return { url, requestCount }
-	}
+	return
 }
-
-
 
 module.exports = {
 	testRequest
--- extension.js
+++ extension.js
@@ -5,8 +5,7 @@ const vscodeExtTest = require('./app')
  * @param {vscode.ExtensionContext} context Context
  */
 async function activate(context) {
-	context.subscriptions.push(vscode.commands.registerCommand('vscodeExtTest.testRequestTryRepro', async () => { await vscodeExtTest.testRequest(true) }))
-	context.subscriptions.push(vscode.commands.registerCommand('vscodeExtTest.testRequestNoRepro', async () => { await vscodeExtTest.testRequest(false) }))
+	context.subscriptions.push(vscode.commands.registerCommand('vscodeExtTest.testRequestTryRepro', vscodeExtTest.testRequest))
 }
 
 function deactivate() { }
